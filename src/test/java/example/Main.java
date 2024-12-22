2244546c9c270f900f2e3b9d8f96b486db709f04
lahirudezoysa <deepthe14@gmail.com>
2024-12-22 12:22:50 +0530

    An update in hibernate framework

--- src/test/java/example/Main.java
+++ src/test/java/example/Main.java
@@ -5,19 +5,24 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+
 
 public class Main {
 
 
     public static void main(String[] args) {
-        Student s1=new Student("lahiru",3.4);
+        Student s1=new Student(1,"kamal",3.4);
+        Student s2=new Student(2,"kamal",3.4);
         Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        Student u1=session.get(Student.class, 1);
+        u1.setSName("lahiru");
+        u1.setGpa(4.0);
+        session.persist(s2);
+        session.getTransaction().commit();
 
     }
 }
--- src/test/java/example/Student.java
+++ src/test/java/example/Student.java
@@ -1,51 +1,19 @@
 package example;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Student {
+    @Id
     private int sId;
     private String sName;
     private Double gpa;
 
-    public Student(int sId, String sName, Double gpa) {
-        this.sId = sId;
-        this.sName = sName;
-        this.gpa = gpa;
-    }
-    public Student( String sName, Double gpa) {
-        this.sId = sId;
-        this.sName = sName;
-        this.gpa = gpa;
-    }
-
-    public Double getGpa() {
-        return gpa;
-    }
-
-    public void setGpa(Double gpa) {
-        this.gpa = gpa;
-    }
-
-    public String getsName() {
-        return sName;
-    }
-
-    public void setsName(String sName) {
-        this.sName = sName;
-    }
-
-    public int getsId() {
-        return sId;
-    }
-
-    public void setsId(int sId) {
-        this.sId = sId;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "sId=" + sId +
-                ", sName='" + sName + '\'' +
-                ", gpa=" + gpa +
-                '}';
-    }
 }
